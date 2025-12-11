abd3ed898528b2fa2d0503a31b0b5c42c6d77b44
Fabri404 <Fabri404lopez@gmail.com>
2025-12-10 22:58:06 -0300

    [ADD] Paginator products

--- game_store/catalogo/views.py
+++ game_store/catalogo/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from .models import Juego
+from django.core.paginator import Paginator
+
 
 def lista_juegos(request):
-    juegos = Juego.objects.all()
-    contexto_catalogo_juegos = {'lista_juegos': juegos}
+    juegos = Juego.objects.all().order_by('id')
+
+    paginator = Paginator(juegos, 6)  # Mostrar 6 juegos por página
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)  
+
+
+    contexto_catalogo_juegos = {'lista_juegos': page_obj}
     return render(request, 'catalogo/lista_juegos.html', contexto_catalogo_juegos)
